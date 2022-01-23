3f4b846dae84ca1a1dd8092d442aef9da0487ece
Anton Fagerholm <anton.fagerholm@gmail.com>
2022-01-23 17:38:53 +0200

    shopping cart functionality added

---
+++ client/src/features/basket/BasketSummary.tsx
@@ -0,0 +1,49 @@
+import {
+  TableContainer,
+  Paper,
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@mui/material";
+import { useStoreContext } from "../../app/context/StoreContext";
+import { currencyFormat } from "../../app/util/util";
+
+export default function BasketSummary() {
+  const { basket } = useStoreContext();
+  const subtotal = basket ? +basket?.items.reduce((sum, item) => sum + item.price * item.quantity, 0) : 0;
+  const deliveryFee = subtotal > 10000 ? 0 : 500;
+
+  //((item.price / 100) * item.quantity).toFixed(2)
+  // reduce function
+
+  return (
+    <>
+      <TableContainer component={Paper} variant={"outlined"}>
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell colSpan={2}>Subtotal</TableCell>
+              <TableCell align="right">{currencyFormat(subtotal)}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell colSpan={2}>Delivery fee*</TableCell>
+              <TableCell align="right">{currencyFormat(deliveryFee)}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell colSpan={2}>Total</TableCell>
+              <TableCell align="right">{currencyFormat(subtotal + deliveryFee)}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <span style={{ fontStyle: "italic" }}>
+                  *Orders over $100 qualify for free delivery
+                </span>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+}
--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,3 +1,5 @@
+
+import { LoadingButton } from "@mui/lab";
 import {
   Avatar,
   Button,
@@ -8,14 +10,29 @@ import {
   CardMedia,
   Typography,
 } from "@mui/material";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import agent from "../../app/api/agent";
+import { useStoreContext } from "../../app/context/StoreContext";
 import { Product } from "../../app/models/product";
+import { currencyFormat } from "../../app/util/util";
 
 interface Props {
   product: Product;
 }
 
 export default function ProductCard({ product }: Props) {
+  const [loading, setLoading] = useState(false);
+  const {setBasket} = useStoreContext();
+
+  function handleAddItem(productId: number) {
+    setLoading(true);
+    agent.Basket.addItem(productId)
+      .then(basket => setBasket(basket))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }
+
   return (
     <Card>
       <CardHeader
@@ -40,15 +57,23 @@ export default function ProductCard({ product }: Props) {
       />
       <CardContent>
         <Typography gutterBottom color="secondary" variant="h5">
-          €{(product.price / 100).toFixed(2)}
+          {currencyFormat(product.price)}
         </Typography>
         <Typography variant="body2" color="text.secondary">
           {product.brand} / {product.type}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Add to cart</Button>
-        <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
+        <LoadingButton
+          loading={loading}
+          onClick={() => handleAddItem(product.id)}
+          size="small"
+        >
+          Add to cart
+        </LoadingButton>
+        <Button component={Link} to={`/catalog/${product.id}`} size="small">
+          View
+        </Button>
       </CardActions>
     </Card>
   );
